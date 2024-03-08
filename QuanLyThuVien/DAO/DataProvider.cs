adbeea50298eb102bb5a45ef228cbe9deaa3259d
thocodeanhquan <thocodeanhquan@gmail.com>
2024-03-08 08:47:46 +0700

    Add project files.

---
+++ QuanLyThuVien/DAO/DataProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QuanLyThuVien.DAO
+{
+    class DataProvider
+    {
+        static string provider = @"Data Source=ANHQUAN;Initial Catalog=QLThuVien;Integrated Security=True";
+        SqlConnection connect = new SqlConnection(provider);
+
+        public DataTable GetData(string sql)
+        {
+            DataTable rs = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(sql, connect);
+            adapter.Fill(rs);
+            return rs;
+        }
+        //public void Excute(string sql)
+        //{
+        //    connect.Open();
+        //    SqlCommand command = new SqlCommand(sql, connect);
+        //    command.ExecuteNonQuery();
+        //    connect.Close();
+        //}
+
+        public void Excute(string sql)
+        {
+            try
+            {
+                connect.Open();
+                SqlCommand command = new SqlCommand(sql, connect);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception, log, or rethrow if needed
+                throw;
+            }
+            finally
+            {
+                // Ensure the connection is closed, whether there was an exception or not
+                if (connect.State == ConnectionState.Open)
+                {
+                    connect.Close();
+                }
+            }
+        }
+
+    }
+}
---
+++ QuanLyThuVien/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.LookAndFeel;
+namespace QuanLyThuVien
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            if(Environment.OSVersion.Version.Major >= 6)
+            {
+                SetProcessDPIAware();
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            DevExpress.Skins.SkinManager.EnableFormSkins();
+            DevExpress.UserSkins.BonusSkins.Register();
+            UserLookAndFeel.Default.SetSkinStyle("Xmas 2008 Blue");
+            Application.Run(new fLogin());
+        }
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+    }
+}
